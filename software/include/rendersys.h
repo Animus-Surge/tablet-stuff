d79c20a14fbe9e41810679ec8c73a86437542935
Surge <esfloyd9341@gmail.com>
2024-09-24 18:34:19 -0400

    Rewrites

--- software/include/rendersys.h
+++ software/include/rendersys.h
@@ -10,38 +10,3 @@ Render subsystem interface
 
 #define DEBUG_SCREEN_WIDTH 1024
 #define DEBUG_SCREEN_HEIGHT 600
-
-extern SDL_Color current_color;
-extern SDL_Window* window;
-extern SDL_Renderer* renderer;
-
-// Create and initialize render system, with window size and title
-void init(int, int, const char*);
-
-// Start the render subsystem
-void run();
-
-// Gracefully terminate the render subsystem
-void shutdown();
-
-// Helpers
-void set_render_color(SDL_Color);
-
-//Flat rendering
-void render_line(int, int, int, int, int);
-void render_line(SDL_Point, SDL_Point, int);
-void render_point(int, int);
-void render_point(SDL_Point);
-void render_circle(int, int, int, int);
-void render_circle(SDL_Point, int, int);
-void render_rect(int, int, int, int, int);
-void render_rect(SDL_Rect, int);
-void render_polygon(SDL_Point[], int, int);
-void render_polyline(SDL_Point[], int, int);
-
-//Fill rendering
-void fill_rect(int, int, int, int);
-void fill_rect(SDL_Rect);
-void fill_circle(int, int, int);
-void fill_circle(SDL_Point, int);
-void fill_polygon(SDL_Point[], int);
--- software/src/app.cpp
+++ software/src/app.cpp
@@ -1,10 +1,4 @@
-#include "app.h"
-#include "rendersys.h"
 
 void init() {
-    init(1024, 600, "Test Window");
 
-    run();
-
-    shutdown();
 }
