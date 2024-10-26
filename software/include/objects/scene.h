510435c7688c897cff2795d0a73fc7f62cb3eb12
Surge <esfloyd9341@gmail.com>
2024-10-26 17:29:27 -0400

    Scene work

--- software/include/objects/scene.h
+++ software/include/objects/scene.h
@@ -28,15 +28,10 @@ public:
         return widgets[name];
     }
 
-    virtual void init() = 0;
     void handleEvent(SDL_Event* event);
     void update();
     void render(SDL_Renderer* renderer);
     void clean();
 };
 
-class BasicTestScene : public Scene {
-public:
-    void init() override;
-};
 
